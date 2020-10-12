ec42d939242fa6c4bfa33abc73b179040f465020
DayDay <daydaybigking@sina.com>
2020-10-12 08:15:08 +0800

    取消使用 browserify, 并修改 readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gulp_simple_test
 一个简单的测试，以学习 gulp 的使用方法
+
+## 测试流程
+- [ x ] 使用 babel 编译 ES6 语法
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 const { src, dest, watch, series } = require("gulp");
 const babel = require("gulp-babel");
 const del = require("delete");
-const browserify = require("browserify");
 
 function clearDist(cb) {
   return del(["dist/*.js", "dist/*.map"], cb);
 }
 
 function defaultTask() {
-  return browserify({
-    entries: "src/index.js",
-  })
-    .bundle()
-    .pipe(src("src/*.js"))
+  return src("src/*.js")
     .pipe(babel({ presets: ["@babel/env"] }))
     .pipe(dest("dist/"));
 }
